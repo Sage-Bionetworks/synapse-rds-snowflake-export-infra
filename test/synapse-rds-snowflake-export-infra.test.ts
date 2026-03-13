1b02d49f59d4c38349819d086ad9b49808c72002
Xavier Schildwachter <x.schildwachter@sagebase.org>
2026-03-13 09:34:24 -0700

    Init

---
+++ test/synapse-rds-snowflake-export-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SynapseRdsSnowflakeExportInfra from '../lib/synapse-rds-snowflake-export-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/synapse-rds-snowflake-export-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SynapseRdsSnowflakeExportInfra.SynapseRdsSnowflakeExportInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
